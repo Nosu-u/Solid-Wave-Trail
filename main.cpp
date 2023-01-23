f41e7ae6004ab17f6024ff547bcf8f8cc7d49d6d
Nosu♡ <93537876+Nosu-u@users.noreply.github.com>
2023-01-23 20:03:58 +0100

    works now lmfao

--- main.cpp
+++ main.cpp
@@ -6,19 +6,19 @@ USE_GEODE_NAMESPACE();
 class $modify(HardStreak) {
 	void updateStroke(float b) { // modifies the wave trail every frame
 		m_isSolid = true; // makes it be a one color
+		this->setBlendFunc(kCCBlendFuncDisable); // removes the blending of it
+		this->setOpacity(255); // so the fade out dont look weird
 		
 		HardStreak::updateStroke(b);
 	}
 };
 
-class $modify(PlayerObject) {
-	void update(float c) {
-		auto thing = getBlendFunc();
+class $modify(PlayLayer) {
+	void destroyPlayer(PlayerObject* p, GameObject* g) {
+		auto fadeOut = CCFadeOut::create(0.01); // creates the fade out because its broken for some reason
 		
-		if (m_waveTrail) {
-			m_waveTrail->setBlendFunc(thing); // removes the blending of it
-		}
+		p->m_regularTrail->runAction(fadeOut); // applies the fade
 		
-		PlayerObject::update(c);
+	PlayLayer::destroyPlayer(p,g);
 	}
 };
